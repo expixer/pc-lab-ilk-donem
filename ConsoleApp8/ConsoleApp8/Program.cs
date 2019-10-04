43b3967eccb28e5f19b6a6fc8e84e170ff7c209b
sezgin <sezginex@gmail.com>
2019-10-04 11:32:05 +0300

    odev1 eklendi

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İşlem yapılacak ilk sayıyı giriniz: ");
+            string ilksayi = Console.ReadLine();
+            Console.Write("İşlem yapılacak ikinci sayıyı giriniz: ");
+            string ikincisayi = Console.ReadLine();
+            int ilksayint = Convert.ToInt32(ilksayi);
+            int ikincisayint = Convert.ToInt32(ikincisayi);
+            double doubleilk = Convert.ToDouble(ilksayi);
+            double doubleikinci = Convert.ToDouble(ikincisayi);
+            Console.WriteLine("Sayıların toplamı: " + (ilksayint + ikincisayint) + " Sayıların Çarpımı: " + (ilksayint * ikincisayint) + " Sayıların ortalaması: " + ((doubleilk + doubleikinci) / 2));
+            int a = 1;
+                }
+    }
+}
