107c6a9a2051639be20c56d81019f202e1b7d181
Yucelpc <sezginex@gmail.com>
2019-10-04 14:55:23 +0300

    ilk hafta düzenlendi

---
+++ Ornek_Kodlar_Hafta1/Ornek_Kodlar_Hafta1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Kodlar_Hafta1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "tartar";
+            string verial = Console.ReadLine() + " deneme";
+            Console.WriteLine("Selam " + isim + " " + verial);
+
+            int a = 5;
+            int b = 3;
+
+            int c = a + b;
+            Console.WriteLine("a+b= " + c);
+        }
+    }
+}
