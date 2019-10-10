aa0885386f7ce7e4f813fc3099dbb8d690d63d55
BMLAB <BMLAB@DESKTOP-GQB64L9>
2019-10-10 11:44:30 +0300

    Prog. Hafta 4 ekleme

---
+++ Programlama_Hafta4/Programlama_Hafta4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programlama_Hafta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t;
+            int i = 123;
+            float f = 23.45F;
+
+            t = typeof(int);
+            Console.Write(t.ToString() + "  ");
+            Console.WriteLine(i.GetType());
+
+            t = typeof(float);
+            Console.WriteLine(t.ToString() + "  ");
+            Console.WriteLine(f.GetType());
+        }
+    }
+}
