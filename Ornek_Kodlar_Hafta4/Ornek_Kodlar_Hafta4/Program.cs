3ce90a29a6c38b26bc648ecc1fc2aef213869987
balikesir.ceng@hotmail.com <balikesir.ceng@hotmail.com>
2019-10-11 11:27:21 +0300

    4.hafta lab

---
+++ Ornek_Kodlar_Hafta4/Ornek_Kodlar_Hafta4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Kodlar_Hafta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*      int n = 4;
+                 int m = 10;
+                 int p = m / n;
+                 float x = m / n;
+                 double y = m / n;
+                Console.WriteLine("p= {0} ", p); ;
+                 Console.WriteLine("x= {0} ", x); ;
+                 Console.WriteLine("y= {0} ", y); ; */
+
+            double x = 10;
+            double y = 4;
+            double sonuc = x % y;
+            Console.WriteLine("x % y = {0}", sonuc);
+
+
+        }
+    }
+}
