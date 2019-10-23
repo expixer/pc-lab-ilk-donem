63228102666129643a82aa70fcddcd47a1b7eaab
Sezgin Sevinç <sezginex@gmail.com>
2019-10-23 20:00:43 +0300

    kur dünzenleme eklendi.

--- lab-odev-2/lab-odev-2/Program.cs
+++ lab-odev-2/lab-odev-2/Program.cs
@@ -10,14 +10,37 @@ namespace lab_odev_2
     {
         static void Main(string[] args)
         {
-            donustur:{ 
-            Console.Write("Dönüştürmek istediğiniz TL mikatrını giriniz: ");
-            double lira = Convert.ToDouble(Console.ReadLine());
-            double dolaresit = lira * 0.1754;
-            double euroesit = lira * 0.1515;
-            Console.Write("Dolar dönüşümü için 1'e Euro dönüşümü için 2'ye basınız: ");
-            int secim = Convert.ToInt32(Console.ReadLine());
-            double sonuc=0;
+        donustur: {
+                double dolarkur = 1 / 5.75;
+                double eurokur = 1 / 6.5;
+                int kursec=0;
+                int secim = 0;
+                Console.Write("Dönüştürmek istediğiniz TL mikatrını giriniz(Kur düzenlemek için 0'a basınız): ");
+                double lira = Convert.ToDouble(Console.ReadLine());
+
+
+                if (lira == 0) {
+                    Console.WriteLine("Dolar kurunu değiştirmek için 1'e Euro kurunu değiştirmek içni 2'ye basınız: ");
+                    kursec= Convert.ToInt32(Console.ReadLine());
+                    if (kursec == 1) { dolarkur = 1 / Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Dönüştürmek istediğiniz TL mikatrını giriniz: ");
+                        lira = Convert.ToDouble(Console.ReadLine());
+                        secim = 1;
+                    }
+                    else if (kursec == 2)
+                    {
+                     eurokur = 2 / Convert.ToDouble(Console.ReadLine());
+                        Console.Write("Dönüştürmek istediğiniz TL mikatrını giriniz: ");
+                        lira = Convert.ToDouble(Console.ReadLine());
+                        secim = 2;
+                    }
+                }
+            double dolaresit = lira * dolarkur;
+            double euroesit = lira * eurokur;
+                if (kursec==0) { Console.Write("Dolar dönüşümü için 1'e Euro dönüşümü için 2'ye basınız: "); 
+             secim = Convert.ToInt32(Console.ReadLine());
+                }
+                double sonuc=0;
             string kur ="";
             if (secim == 1) { sonuc = dolaresit; kur = "dolar"; }
             else if (secim == 2) { sonuc = euroesit; kur = "euro"; }
