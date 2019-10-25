2f8cf3d0f0c8e860800b1280e40f5b02348d87ee
balikesir.ceng@hotmail.com <balikesir.ceng@hotmail.com>
2019-10-25 10:45:46 +0300

    İlk örnekler eklendi.

---
+++ Ornek_Kodlar_Hafta6/Ornek_Kodlar_Hafta6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Kodlar_Hafta6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Para miktarını giriniz: ");
+            int para = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dolar için 1 Euro için 2 giriniz: ");
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            switch (secim)
+            {
+                case 1:
+                    Console.WriteLine("{0} TL = {1} Dolar", para, para / 5);
+                    break;
+                case 2:
+                    Console.WriteLine("{0} TL = {1} Euro", para, para / 7);
+                    break;
+            
+            }
+
+                
+        }
+    }
+}
