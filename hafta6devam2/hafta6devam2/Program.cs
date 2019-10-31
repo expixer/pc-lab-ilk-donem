2e0c40dc5c333910bb03e1ea220fd968e7e7afeb
Sezgin Sevinc <sezginex@gmail.com>
2019-10-31 11:44:53 +0300

    sayı toplama eklendi

---
+++ hafta6devam2/hafta6devam2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hafta6devam2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, toplam = 0, BirOncekiSayi = 0;
+            for (; ; )
+            {
+                Console.Write("Bir sayı girin: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                if (sayi == 0)
+                    break;
+                if (sayi == BirOncekiSayi)
+                {
+                    Console.WriteLine("Farklı bir sayı girin");
+                    continue;
+
+                }
+                toplam += sayi;
+                BirOncekiSayi = sayi;
+            }
+            Console.WriteLine("Toplam{0} ", toplam);
+        }
+    }
+}
