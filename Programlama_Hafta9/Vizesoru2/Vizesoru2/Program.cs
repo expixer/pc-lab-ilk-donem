67b251767c1d9abae3f5228c8233223bbf208cd5
BMLAB <BMLAB@DESKTOP-GQB64L9>
2019-11-14 10:06:56 +0300

    vize 2. soru eklendi

---
+++ Programlama_Hafta9/Vizesoru2/Vizesoru2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizesoru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+            int gir = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= gir; i++)
+            {
+                toplam += i * i * (i + 1);
+            }
+            Console.WriteLine(toplam);
+        }
+    }
+}
