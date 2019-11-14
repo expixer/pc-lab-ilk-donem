195f43f7a86b6bb60f36d763291d4ddac9c8b79d
BMLAB <BMLAB@DESKTOP-GQB64L9>
2019-11-14 10:23:11 +0300

    Vize soru 4 eklendi

---
+++ Programlama_Hafta9/Vizesoru4/Vizesoru4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizesoru4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cift, tek;
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi % 2 == 0)
+                for (int i = 1; i <= 50; i++)
+                {
+                    cift = sayi - (i * 2);
+                    Console.WriteLine(cift);
+                }
+            else
+                for (int j = 1; j <= 50; j++)
+                {
+                    tek = sayi + (j * 2);
+                    Console.WriteLine(tek);
+                }
+        }
+
+    }
+}
