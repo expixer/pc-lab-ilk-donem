1de6959a9925833903d3f777027fcc927d17794d
balikesir.ceng@hotmail.com <balikesir.ceng@hotmail.com>
2019-12-06 11:22:04 +0300

    ders

---
+++ aykürt/aykürt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aykürt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac=0;
+            int[] sayi = { 1, 2, 3, 4, 5, 3, 3, 3, 5, 4 };
+            int kontrol = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < sayi.Length; i++)
+                if (kontrol == sayi[i])
+                    sayac++;
+            Console.WriteLine("Verdiğiniz sayı " +sayac+" defa var");
+        }
+    }
+}
