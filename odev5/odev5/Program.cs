213b3b2c86c9621623870630b61bb1c225815308
Sezgin Sevinc <sezginex@gmail.com>
2019-12-27 11:13:49 +0300

    odev5

---
+++ odev5/odev5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev5
+{
+    class Program
+    {
+
+        static void dikdortgen(int x, int y)
+
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kısa Kenarı girin");
+            int kisa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Uzun Kenarı girin");
+            int uzun = Convert.ToInt32(Console.ReadLine());
+            dikdortgen(kisa, uzun);
+        }
+
+    }
+}
